48375e3c786d89abcf76a76a1c7e95ba11b20fe5
Dragos Suciu <73345838+dragos5436@users.noreply.github.com>
2025-06-06 19:17:13 +0100

    Update get_other_language() function

--- lib.php
+++ lib.php
@@ -1793,7 +1793,8 @@ function get_other_languages($export=false, $lang=null, $userid=null) {
     }
     // Add translated language name for each other language
     foreach ($data as $language) {
-        $language->label = get_string_from_language($lang, 'language.' . $language->language, 'artefact.europass');
+        $language->languagetag = 'language.' . $language->description;
+        $language->label = get_string_from_language($lang, $language->languagetag, 'artefact.europass');
     }
 
     // Add language certificate and experience info to each language
